3bb9da8aadec56c642a051d95660b63883c7f0b6
AndresM9406 <87483102+AndresM94@users.noreply.github.com>
2023-02-23 18:25:41 -0600

    Update taller.py
    finalizando todas las funciones y soluciones del taller

--- condicionales/taller.py
+++ condicionales/taller.py
@@ -1,7 +1,4 @@
 #Solcucion punto 1:
-#valor1 = int( input("Digite el valor de x: ") )
-#valor2 = int( input("Digite el valor de y: ") )
-
 def ecuacion(x, y):
     formula1 = x + 3 * y
     formula2 = 2 * x - 5 * y
@@ -10,19 +7,7 @@ def ecuacion(x, y):
     else:
         print("Es incorrecto")
 
-
-#ecuacion(valor1, valor2)
-
 #solucion punto 2:
-
-#hora1 = int( input("Digite la hora del primero 1 a 12:\n") )
-#minutos1 = int( input("Digite los minutos\n") )
-#meridiano1 = input("Diprint(result)gite si es AM o PM:\n")
-
-#hora2 = int( input("Digite la hora del segundo 1 a 12:\n") )
-#minutos2 = int( input("Digite los minutos del segundo:\n") )
-#meridiano2 = input("Digite si es AM o PM:\n")
-
 def horasFormato12(h1, m1, mer1, h2, m2, mer2):
     if 0 < h1 <= 12 and 0 < h2 <= 12 and 0 <= m1 <= 59 and 0 <= m2 <= 59:
         if mer1.upper() == mer2.upper():
@@ -38,21 +23,8 @@ def horasFormato12(h1, m1, mer1, h2, m2, mer2):
             print(f"La hora {h1}:{m1} {mer1} es menor a {h2}:{m2} {mer2}")
     else:
         print("El formato no existe")
-#    elif h1 >= h2 and m1 >= m2:
-#       print(f"la hora {h1}:{m1} {mer1} es mayor a {h2}:{m2} {mer2}")
-#    else:
-#        print(f"La hora {h1}:{m1} {mer1} es menor a {h2}:{m2} {mer2}")
-        
-#horasFormato12(hora1, minutos1, meridiano1, hora2, minutos2, meridiano2)
 
 #solucion punto 3:
-
-#print("Ingrese las horas en formato militar:")
-#hora1 = int( input("Ingrese la hora 1: ") )
-#min1 = int( input("Ingrese el minuto 1: ") )
-#hora2 = int( input("Ingrese la hora 2: ") )
-#min2 = int( input("Ingrese el minuto 2: ") )
-
 def formato24H(h1, m1, h2, m2):
     if 0 <= h1 <= 23 and 0 <= h2 <= 23 and 0 <= m1 <= 59 and 0 <= m2 <= 59:
         if h1 == h2 and m1 == m2:
@@ -67,12 +39,9 @@ def formato24H(h1, m1, h2, m2):
     else:
         print("Formato no valido")   
 
-#formato24H(hora1, min1, hora2, min2)
+
 
 #Solucion punto 4:
-
-numero = float( input("Digite un numero decimal: ") )
-
 def redondeo(num):
     result = int(num)
     if num > result + 0.5:
@@ -80,6 +49,72 @@ def redondeo(num):
     else:
         print(result)
         
-redondeo(numero)
+
 
 #Solucion punto 5:
+def dibujar(dibujo):
+    if dibujo == 1:
+        print("""
+        * * * * *
+        *       *
+        *       *
+        * * * * *""")
+    elif dibujo == 2:
+        print("""
+        * * * * * *
+        *         *
+        *         *
+        * * * * * *""")
+    elif dibujo == 3:
+        print("""
+                *
+              *   *
+            *       *
+          *           *
+        * * * * * * * * *""")
+    elif dibujo == 4:
+        print("* * * * * * * * *")
+
+solucion = int( input("Elige la el numero de la solucion que desea probar del 1 al 5:\n") )
+if solucion == 1:
+    #Solcucion punto 1:
+    valor1 = int( input("Digite el valor de x: ") )
+    valor2 = int( input("Digite el valor de y: ") )
+    #llamo a la funcion y le paso como parametro cada variable de entrada
+    ecuacion(valor1, valor2)
+elif solucion == 2:
+    #solucion punto 2:
+    hora1 = int( input("Digite la hora del primero 1 a 12:\n") )
+    minutos1 = int( input("Digite los minutos\n") )
+    meridiano1 = input("Diprint(result)gite si es AM o PM:\n")
+
+    hora2 = int( input("Digite la hora del segundo 1 a 12:\n") )
+    minutos2 = int( input("Digite los minutos del segundo:\n") )
+    meridiano2 = input("Digite si es AM o PM:\n")
+    #llamo a la funcion y le paso como parametro cada variable de entrada
+    horasFormato12(hora1, minutos1, meridiano1, hora2, minutos2, meridiano2)
+elif solucion == 3:
+    #solucion punto 3:
+    print("Ingrese las horas en formato militar:")
+    hora1 = int( input("Ingrese la hora 1: ") )
+    min1 = int( input("Ingrese el minuto 1: ") )
+    hora2 = int( input("Ingrese la hora 2: ") )
+    min2 = int( input("Ingrese el minuto 2: ") )
+    #llamo a la funcion y le paso como parametro cada variable de entrada
+    formato24H(hora1, min1, hora2, min2)
+elif solucion == 4:
+    #Solucion punto 4:
+    numero = float( input("Digite un numero decimal: ") )
+    #llamo a la funcion y le paso como parametro cada variable de entrada
+    redondeo(numero)
+elif solucion == 5:
+    #Solucion punto 5:
+    eleccion = int( input("""Elige el numero de la figura que desea dibujuar:
+  1. Cuadrado
+  2. Rectangulo
+  3. Triangulo
+  4. Linea\n""") )
+    #llamo a la funcion y le paso como parametro cada variable de entrada
+    dibujar(eleccion)
+else:
+    print(f"Solucion #{solucion} no existe")
